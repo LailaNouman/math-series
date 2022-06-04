3c3379553534ed9d8cd109ea058b79c5145ca093
unknown <lailaibraheem@hotmail.com>
2022-06-04 23:37:49 +0300

    edited

--- series.py
+++ series.py
@@ -1,10 +1,14 @@
 def fibonacci(n):
+    # Integer series starts with 0 and 1
+    # the next integer is the summation of the previous two.
    if n == 0 or n == 1:
        return n
    else:
        return fibonacci(n-1) + fibonacci(n-2)
 
 def lucas(n):
+    # Integer series starts with 2 and 1
+    # the next integer is the summation of the previous two.
    if n == 0:
        return 2
    elif n == 1:
@@ -13,6 +17,9 @@ def lucas(n):
        return lucas(n-1) + lucas(n-2)
 
 def sum_series(x, y = 0, z = 1):
+    # Integer series starts with x and y, the default value for them
+    # is x = 0, y = 1, the next integer is the summation
+    # of the previous two.
     if x == 0:
         return y
     elif x == 1:
